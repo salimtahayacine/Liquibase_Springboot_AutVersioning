e5f4ea13eafb203c4c2b70083bafc432e3b37d95
salimtahayacine <salimtahayacine@gmail.com>
2025-01-16 10:49:19 +0100

    first commit

---
+++ src/main/java/com/stronglover/Springboot/Liquibase/entity/User.java
@@ -0,0 +1,33 @@
+package com.stronglover.Springboot.Liquibase.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String adress;
+    private String password;
+
+    private String tel;
+
+    private String test_liquibase;
+}
---
+++ src/test/java/com/stronglover/Springboot/Liquibase/SpringbootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stronglover.Springboot.Liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
